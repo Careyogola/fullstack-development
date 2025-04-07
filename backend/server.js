c8430bba69e4b014c51cb79ba45ba1d0fe648e5f
Carey Ogola <ogolacarey254@gmail.com>
2025-04-07 15:21:20 +0300

    Backend server Done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
 import productRoutes from './routes/routes.js'
 
 dotenv.config();
@@ -12,7 +10,8 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 
 // console.log(process.env.MONGO_URL);
+const port = process.env.PORT || 4001;
 
-app.listen(5500, ()=>{
+app.listen(port, ()=>{
     connectDB();
 });
