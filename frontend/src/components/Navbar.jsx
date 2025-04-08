64a748a04e7b1d0732dec46b458d5a8bf98de1d9
Carey Ogola <ogolacarey254@gmail.com>
2025-04-08 09:09:57 +0300

    Navlinks resolved , issues resolved

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,16 +6,34 @@ const Navbar = () => {
       <div className='flex flex-row justify-between'>
       <div className='flex flex-row gap-4 justify-evenly items-center text-black'>
         <a href='/' className='text-2xl font-bold'>Created<span className='text-teal-900'>Store!</span></a>
-        <a href='/products'>Products</a>
-        <a href='/docs'>Docs</a>
-        <a href='/deleted'>DeletedProducts</a>
-        <a href='/updated'>UpdatedProducts</a>
+        <a href='/products' className="group text-black transition duration-300">Products
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href='/docs' className="group text-black transition duration-300">Docs
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href='/deleted' className="group text-black transition duration-300">DeletedProducts
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href='/updated' className="group text-black transition duration-300">UpdatedProducts
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href="#" className="group text-black transition duration-300">Link
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+
       </div>
 
       <div className='flex flex-row justify-evenly items-center gap-4'>
-        <a href='/signin'>Sign In</a>
-        <a href='/signup'>Create Account</a>
-        <a href='/contact'>Contact Sales</a>
+        <a href='/signin' className="group text-black transition duration-300">Sign In
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href='/signup' className="group text-black transition duration-300">Create Account
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
+        <a href='/contact'className="group text-black transition duration-300">Contact Sales
+        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+        </a>
       </div>
       </div>
     </div>
