ea70c6060ec9bbaea008ea806d337d1ca98afd34
Carey Ogola <ogolacarey254@gmail.com>
2025-04-10 10:43:57 +0300

    created another get api to get all users created ,working

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -74,3 +74,16 @@ export const logout = async (req, res) => {
         data: "Youve hit signout endpoint."
     })
 }
+
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({
+            success: true,
+            data: users,
+        })
+    } catch (error) {
+        
+    }
+}
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
-import { login, logout, signup } from '../controllers/authController.js';
+import { login, logout, signup, getUsers } from '../controllers/authController.js';
+import { get } from 'http';
 
 const router = express.Router();
 
 router.post('/signup', signup)
 router.post('/login', login)
 router.post('/logout',logout)
+
+router.get('/users', getUsers)
 
 export default router;
